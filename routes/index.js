2c590a9fe67075c1900b42447f59e78d1b742d31
huyduong1589 <minhhuyduong89@gmail.com>
2022-03-18 20:55:10 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NC DEVOPS' });
+  res.render('index', { title: 'NC DEVOPS 2022' });
 });
 
 module.exports = router;
