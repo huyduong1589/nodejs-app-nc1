47f430e6ebca1bb1384664cb9c3a61da4059a990
huyduong1589 <minhhuyduong89@gmail.com>
2022-03-18 20:34:58 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Jenkins' });
+  res.render('index', { title: 'NC DEVOPS' });
 });
 
 module.exports = router;
