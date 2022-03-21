7b0b1817c43dc959b0732dfeca70d7ae6d7e18dd
huyduong1589 <minhhuyduong89@gmail.com>
2022-03-21 20:24:29 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jenkins Session 2' });
+  res.render('index', { title: 'Jenkins Session 3' });
 });
 
 module.exports = router;
